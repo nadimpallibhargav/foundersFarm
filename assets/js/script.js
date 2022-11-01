56299767b4925f64f270151e72678c2985357896
bhargav27b <bhargav@sketchbrahma.com>
2022-11-01 23:23:00 +0530

    zoho forms

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,3 +129,157 @@ function scrollToSmoothly(obj, time) {
     });
   }, interval);
 }
+
+
+(function() {
+	try{
+
+			if( document.readyState == "complete" ){ 
+				onloadActions_424835();
+			}  else {
+			  	window.addEventListener('load', function (){
+			  		onloadActions_424835();
+			  	}, false);
+			}
+
+			function onloadActions_424835(){
+				constructDiv_424835();
+				
+			}
+
+			function constructDiv_424835(){
+				var iframeDiv = document.createElement("div");
+				iframeDiv.setAttribute('id','xszq2zRLww-ZW3r8cEOjFV93LOl6Uw5H9X8Hk4zYx7s_424835');
+				iframeDiv.setAttribute('class','zf_main_id_424835');
+
+				var closeFormDiv = document.createElement("div");
+				closeFormDiv.setAttribute('id','deleteform_424835');
+				closeFormDiv.setAttribute('class','zf_lb_closeform_424835');
+				
+
+				var containerDiv = document.createElement("div");
+				containerDiv.setAttribute('id','containerDiv_424835');
+				containerDiv.setAttribute('class','zf_lB_Container_424835 fadeIn');
+				containerDiv.appendChild(iframeDiv);
+				containerDiv.appendChild(closeFormDiv);
+				
+				var wrapperDiv = document.createElement("div");
+				wrapperDiv.setAttribute('class','zf_lB_Wrapper_424835');
+				wrapperDiv.appendChild(containerDiv);
+
+
+				var dimmerDiv = document.createElement("div");
+				dimmerDiv.setAttribute('class','zf_lB_Dimmer_424835');
+				dimmerDiv.setAttribute('elname','popup_box');
+
+				var mainDiv = document.createElement("div");
+				mainDiv.setAttribute('id','formsLightBox_424835');
+				mainDiv.style.display = "none";
+				mainDiv.appendChild(wrapperDiv);
+				mainDiv.appendChild(dimmerDiv);
+
+				document.body.appendChild(mainDiv);
+
+			}
+
+			function showZForm_424835(){
+				var iframe = document.getElementById("xszq2zRLww-ZW3r8cEOjFV93LOl6Uw5H9X8Hk4zYx7s_424835").getElementsByTagName("iframe")[0];
+				if(iframe == undefined ||iframe.length == 0){
+					loadZForm_424835();
+					
+				} 
+				document.getElementById("formsLightBox_424835").style.display = "block"; 
+				document.body.style.overflow = "hidden";
+			}
+
+			function loadZForm_424835() {
+				var iframe = document.getElementById("xszq2zRLww-ZW3r8cEOjFV93LOl6Uw5H9X8Hk4zYx7s_424835").getElementsByTagName("iframe")[0];
+				if(iframe == undefined ||iframe.length == 0){
+					var f = document.createElement("iframe");
+					f.src = getsrcurlZForm_424835('https://forms.zohopublic.in/bhargavraju98/form/BookYourVisit/formperma/xszq2zRLww-ZW3r8cEOjFV93LOl6Uw5H9X8Hk4zYx7s?zf_rszfm=1');
+					f.style.border="none";
+					f.style.minWidth="100%";
+					f.style.overflow="hidden";
+					var d = document.getElementById("xszq2zRLww-ZW3r8cEOjFV93LOl6Uw5H9X8Hk4zYx7s_424835");
+					d.appendChild(f);
+
+					var deleteForm = document.getElementById("deleteform_424835");
+					deleteForm.onclick = function deleteZForm_424835() {
+						var divCont = document.getElementById("formsLightBox_424835");
+						divCont.style.display="none";
+						document.body.style.overflow = "";
+
+						var iframe = document.getElementById("xszq2zRLww-ZW3r8cEOjFV93LOl6Uw5H9X8Hk4zYx7s_424835").getElementsByTagName("iframe")[0];
+						iframe.remove();
+					}
+
+					
+
+					window.addEventListener('message', function (){
+						var evntData = event.data;
+						if( evntData && evntData.constructor == String ){
+							var zf_ifrm_data = evntData.split("|");
+							if ( zf_ifrm_data.length == 2 ) {
+								var zf_perma = zf_ifrm_data[0];
+								var zf_ifrm_ht_nw = ( parseInt(zf_ifrm_data[1], 10) + 15 ) + "px";
+								var iframe = document.getElementById("xszq2zRLww-ZW3r8cEOjFV93LOl6Uw5H9X8Hk4zYx7s_424835").getElementsByTagName("iframe")[0];
+								if ( (iframe.src).indexOf('formperma') > 0 && (iframe.src).indexOf(zf_perma) > 0 ) {
+									var prevIframeHeight = iframe.style.height;
+									if ( prevIframeHeight != zf_ifrm_ht_nw ) {
+									iframe.style.minHeight = zf_ifrm_ht_nw;
+										var containerDiv = document.getElementById("containerDiv_424835");
+										containerDiv.style.height=zf_ifrm_ht_nw;
+									}
+								}
+							}
+						}
+
+					}, false);
+				}
+			}
+
+			
+
+			function getsrcurlZForm_424835(zf_src) {
+				try {
+					if ( typeof ZFAdvLead !== "undefined" && typeof zfutm_zfAdvLead !== "undefined" ) {
+						for( var prmIdx = 0 ; prmIdx < ZFAdvLead.utmPNameArr.length ; prmIdx ++ ) {
+				        	var utmPm = ZFAdvLead.utmPNameArr[ prmIdx ];
+				        	var utmVal = zfutm_zfAdvLead.zfautm_gC_enc( ZFAdvLead.utmPNameArr[ prmIdx ] );
+					        if ( typeof utmVal !== "undefined" ) {
+					          if ( utmVal != "" ){
+					            if(zf_src.indexOf('?') > 0){
+					              zf_src = zf_src+'&'+utmPm+'='+utmVal;//No I18N
+					            }else{
+					              zf_src = zf_src+'?'+utmPm+'='+utmVal;//No I18N
+					            }
+					          }
+					        }
+				      	}
+					}
+
+					if ( typeof ZFLead !== "undefined" && typeof zfutm_zfLead !== "undefined" ) {
+						for( var prmIdx = 0 ; prmIdx < ZFLead.utmPNameArr.length ; prmIdx ++ ) {
+				        	var utmPm = ZFLead.utmPNameArr[ prmIdx ];
+				        	var utmVal = zfutm_zfLead.zfutm_gC_enc( ZFLead.utmPNameArr[ prmIdx ] );
+					        if ( typeof utmVal !== "undefined" ) {
+					          if ( utmVal != "" ){
+					            if(zf_src.indexOf('?') > 0){
+					              zf_src = zf_src+'&'+utmPm+'='+utmVal;//No I18N
+					            }else{
+					              zf_src = zf_src+'?'+utmPm+'='+utmVal;//No I18N
+					            }
+					          }
+					        }
+				      	}
+					}
+				}catch(e){}
+				return zf_src;
+			}
+			var buttonElem = document.getElementById("zf_button_424835");
+			buttonElem.style.display = "block";
+			buttonElem.addEventListener("click", showZForm_424835);
+		
+			
+	}catch(e){}
+})();
